dadaae30bfc479741f5ac55c4ee416057fa310db
riccardoforzan <37451892+riccardoforzan@users.noreply.github.com>
2023-06-08 11:10:19 +0200

    Update extract stream

--- scripts/extract_streaming.py
+++ scripts/extract_streaming.py
@@ -1,68 +1,143 @@
 import os
 import json
 import queue
+import logging
 import argparse
 import lightrdf
+from slugify import slugify
 from statistics import mean
+from datetime import datetime
 from collections import defaultdict
 
 SIZE_LIMIT = 200 * 1024 * 1024  # 200 MB
+
+
+def clean_string(s: str) -> str:
+    return " ".join(s.split()).encode("unicode_escape").decode("unicode_escape")
 
 
 def is_literal(node: str) -> bool:
     return node.startswith('"') and node.endswith('"')
 
 
-def process_document(file_path: str) -> dict:
-    doc = lightrdf.RDFDocument(file_path)
+def process_triples(
+    file_path: str,
+    entities_file: str,
+    properties_file: str,
+    literals_file: str,
+    classes_file: str,
+) -> dict:
+    with (
+        open(entities_file, "w+") as e_out,
+        open(properties_file, "w+") as p_out,
+        open(literals_file, "w+") as l_out,
+        open(classes_file, "w+") as c_out,
+    ):
+        doc = lightrdf.RDFDocument(file_path)
 
-    entities = list()
-    properties = list()
-    literals = list()
-    classes = list()
+        number_of_connections = 0
+        vertices_count_literals = defaultdict(int)
 
-    number_of_connections = 0
-    vertices_count_literals = defaultdict(int)
+        for triple in doc.search_triples(None, None, None):
+            sub = triple[0]
+            prop = triple[1]
+            obj = triple[2]
 
-    for triple in doc.search_triples(None, None, None):
-        sub = triple[0]
-        prop = triple[1]
-        obj = triple[2]
+            print(clean_string(sub), file=e_out)
 
-        entities.append(sub)
+            if "type" in prop.lower() or "a" == prop.lower():
+                print(obj, file=c_out)
+                continue
 
-        if "type" in prop.lower() or "a" == prop.lower():
-            classes.append(obj)
-            continue
+            print(clean_string(prop), file=p_out)
 
-        properties.append(prop)
+            is_obj_literal = is_literal(obj)
 
-        is_obj_literal = is_literal(obj)
+            if is_obj_literal:
+                obj_repr = clean_string(eval(obj))
+                if len(obj_repr) > 0:
+                    print(obj_repr, file=l_out)
 
-        if is_obj_literal:
-            literals.append(obj)
+            if not is_obj_literal:
+                print(clean_string(obj), file=e_out)
+                number_of_connections += 1
 
-        if not is_obj_literal:
-            entities.append(obj)
-            number_of_connections += 1
+            # count literals for each vertex
+            if sub not in vertices_count_literals:
+                vertices_count_literals[sub] = 0
 
-        if sub not in vertices_count_literals:
-            vertices_count_literals[sub] = 0
+            vertices_count_literals[sub] += 1
 
-        vertices_count_literals[sub] += 1
+        connected_vertices = len(vertices_count_literals.keys())
+        average_literals_per_vertex = mean(vertices_count_literals.values())
 
-    connected_vertices = len(vertices_count_literals.keys())
-    average_literals_per_vertex = mean(vertices_count_literals.values())
+        return {
+            "connections": number_of_connections,
+            "connected_vertices": connected_vertices,
+            "average_literals_per_vertex": average_literals_per_vertex,
+        }
 
-    return {
-        "classes": classes,
-        "literals": literals,
-        "entities": entities,
-        "properties": properties,
-        "connections": number_of_connections,
-        "connectedVertices": connected_vertices,
-        "averageLiteralsPerVertex": average_literals_per_vertex,
+
+def process_file(dataset_folder: str, dataset: str, file: str):
+    # file to be analyzed
+    base_dataset_path = f"{dataset_folder}/{dataset}"
+    file_path = f"{base_dataset_path}/{file}"
+    metadata_file_path = f"{base_dataset_path}/metadata.json"
+
+    # create output file names
+    base_name = f"{datetime.now().strftime('%Y%m%d-%H%M%S')}-{slugify(file)}"
+    entities_file = f"{base_name}-entities.txt"
+    properties_file = f"{base_name}-properties.txt"
+    literals_file = f"{base_name}-literals.txt"
+    classes_file = f"{base_name}-classes.txt"
+
+    data = None
+
+    try:
+        data = process_triples(
+            file_path,
+            f"{dataset_folder}/{dataset}/{entities_file}",
+            f"{dataset_folder}/{dataset}/{properties_file}",
+            f"{dataset_folder}/{dataset}/{literals_file}",
+            f"{dataset_folder}/{dataset}/{classes_file}",
+        )
+    except Exception as e:
+        log.error(f"{file_path} cannot be parsed: {str(e)}")
+
+        # If an error occurs delete all the associated files
+        for file in [entities_file, properties_file, literals_file, classes_file]:
+            if os.path.exists(file):
+                os.remove(file)
+
+        return
+
+    log.info(f"{file_path} processed")
+
+    # create representation for the parsed dataset
+    entry = {
+        "file": file,
+        "size": os.path.getsize(file_path),
+        "classesFile": classes_file,
+        "literalsFile": literals_file,
+        "entitiesFile": entities_file,
+        "propertiesFile": properties_file,
+        "connections": data["connections"],
+        "connectedVertices": data["connected_vertices"],
+        "averageLiteralsPerVertex": data["average_literals_per_vertex"],
+        "extractedWith": "lightrdf",
     }
+
+    with open(metadata_file_path, "r+") as mf:
+        data = json.load(mf, strict=False)
+
+        data["unusedFiles"].remove(file)
+        data["extracted"].append(entry)
+
+        new_content = json.dumps(data, ensure_ascii=False, indent=4)
+
+        mf.seek(0)
+        mf.truncate(0)
+        mf.write(new_content)
 
 
 if __name__ == "__main__":
@@ -71,6 +146,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset_folder = args.folder
+
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="extract_stream.log",
+        filemode="w",
+        format="%(asctime)-15s %(levelname)-8s %(message)s",
+    )
+
+    log = logging.getLogger()
 
     # Files that have to be processed as streaming
     files_to_process = queue.Queue()
@@ -94,41 +178,4 @@ if __name__ == "__main__":
     # Process the jobs from the queue sequentially
     while not files_to_process.empty():
         dataset, file = files_to_process.get()
-
-        file_path = f"{dataset_folder}/{dataset}/{file}"
-        metadata_file = f"{dataset_folder}/{dataset}/metadata.json"
-
-        data = None
-
-        try:
-            data = process_document(file_path)
-        except Exception as e:
-            print(f"ERROR while processing {file_path}")
-            continue
-
-        print(f"Processed file {file} for dataset {dataset}")
-
-        # represent the data extracted from this file
-        representation = dict()
-        representation["file"] = file
-        representation["fileSize"] = os.path.getsize(file_path)
-        representation = representation | data
-        representation["extractedWith"] = "lightrdf"
-
-        with open(metadata_file, "r+") as f:
-            # read JSON object from the file
-            metadata = json.load(f, strict=False)
-
-            # remove the file from the unused ones
-            metadata["unusedFiles"].remove(file)
-
-            # add the content extracted for the file
-            metadata["extracted"].append(representation)
-
-            # update the content of the metadata
-            content = json.dumps(metadata, ensure_ascii=False, indent=4)
-
-            # write to `metadata.json` with the new extracted data
-            f.seek(0)  # return to the top of the file
-            f.truncate(0)
-            f.write(content)
+        process_file(dataset_folder, dataset, file)
